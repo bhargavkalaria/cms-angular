1039ecdf20f2b07fa99695abeb5dc4a7a656407a
bhargav <bkalaria6191@gmaail.com>
2020-06-07 19:43:29 +0530

    WIP campaign create

--- src/app/dashboard/campaign/campaign-add-edit/campaign-add-edit.component.ts
+++ src/app/dashboard/campaign/campaign-add-edit/campaign-add-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-campaign-add-edit',
@@ -6,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./campaign-add-edit.component.scss']
 })
 export class CampaignAddEditComponent implements OnInit {
+  campaign: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.campaign = this.formBuilder.group({
+      CampaignName: [null, [Validators.required]],
+      CampaignOwner: [null, [Validators.required]],
+      Start_Date: [null, [Validators.required]],
+      End_Date: [null, [Validators.required]],
+      CampaignBudget: [null, [Validators.required]],
+      ExpectedRevenue: [null, [Validators.required]],
+      CreatedOn: [null, [Validators.required]],
+      ModifiedOn: [null, [Validators.required]],
+      CreatedBy: [null, [Validators.required]],
+      CampaignStatusId: [null, [Validators.required]],
+      MarketingTypeId: [null, [Validators.required]],
+      MarketingStrategyId: [null, [Validators.required]],
+      TemplateId: [null, [Validators.required]],
+      BrandId: [null, [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {
   }
